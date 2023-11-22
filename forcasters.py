45d8d178feb8b942a91a4baa5bcdcf5772dd1e13
TTonnyy789 <109502468+TTonnyy789@users.noreply.github.com>
2023-11-22 21:40:19 +0000

    file re organizing
    adjust the files from other teammates

--- forcasters.py
+++ forcasters.py
@@ -3,15 +3,17 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
+import lightgbm as lgb
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, TimeSeriesSplit
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import accuracy_score, mean_squared_error, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, mean_squared_error, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay, r2_score
 
 
 class ZeroSalesForecaster:
     def predict(self, periods):
         return np.zeros(periods)
+
 
 
 class StationarySalesForecaster:
@@ -26,7 +28,7 @@ class StationarySalesForecaster:
     segmented_data = {}
     # Grouping the data by store and product, and storing each group in the dictionary
     for (store, product_type), group in product.groupby(['store_nbr', 'product_type'], observed=True):
-            segmented_data[(store, product_type)] = group[['sales', 'special_offer', 'id']]
+            segmented_data[(store, product_type)] = group[['sales', 'special_offer', 'id', 'store_nbr']]
 
     # Initial the self values in the StationarySalesForecaster class
     def __init__(self, store_number, product_type, order=(8, 2, 6), train_end_date='2016-12-31', validation_end_date='2017-07-31'):  # self.segmented_data = segmented_data
@@ -84,9 +86,8 @@ class StationarySalesForecaster:
         for lag in range(1, lags + 1):
             specific_segment[f'lag_{lag}'] = specific_segment['sales'].shift(lag)
         self.segmented_data[(self.store_number, self.product_type)] = specific_segment
-
     
-    def randomforest_predict(self, lags=12):
+    def randomforest_date_predict(self, lags=12):
         from sklearn.ensemble import RandomForestRegressor
         self.create_lag_features(lags=lags)
         
@@ -103,7 +104,7 @@ class StationarySalesForecaster:
         X_test = test_data[feature_cols]
 
         # Train Random Forest model
-        rf_model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=1)
+        rf_model = RandomForestRegressor(n_estimators=100, max_depth=10, random_state=42)
         rf_model.fit(X_train, y_train)
         rf_predictions = rf_model.predict(X_test)
 
@@ -111,17 +112,17 @@ class StationarySalesForecaster:
         plt.figure(figsize=(12,6))
         plt.plot(test_data.index, rf_predictions, color='green', label='RF Predicted Sales')
         plt.plot(test_data.index, test_data['sales'], color='red', label='Actual Sales')
-        plt.title('Sales Forecast vs Actuals')
+        plt.title(f'Random Forest Sales Forecast vs Actuals for Store {self.store_number} - Product {self.product_type}')
         plt.xlabel('Date')
         plt.ylabel('Sales')
         plt.legend()
         plt.show()
 
         # Model evaluation
-        mse_rf = mean_squared_error(test_data['sales'], rf_predictions)
-        print("Random Forest Mean Squared Error:", mse_rf)
+        r2_rf = r2_score(test_data['sales'], rf_predictions)
+        print("Random Forest r2_score:", r2_rf)
     
-    def optimize_randomforest(self, max_lag=12, cv_splits=3):
+    def optimize_date_randomforest(self, max_lag=12, cv_splits=3):
         from sklearn.ensemble import RandomForestRegressor
         from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV, TimeSeriesSplit
         # Best parameters initialization
@@ -166,6 +167,84 @@ class StationarySalesForecaster:
         print(f"Best MSE: {best_mse}")
         print(f"Best Lag: {best_lag}")
         print(f"Best Parameters: {best_params}")
+    
+    def lightgbm_date_predict(self, lags=12):
+        import lightgbm as lgb
+
+        # Create lag features
+        self.create_lag_features(lags)
+        specific_segment = self.segmented_data[(self.store_number, self.product_type)]
+
+        # Prepare the features and target variable
+        lag_columns = [f'lag_{i}' for i in range(1, lags + 1)]
+        X_train = specific_segment[:self.validation_end_date][lag_columns].values
+        Y_train = specific_segment[:self.validation_end_date]['sales']
+        
+        X_test = specific_segment[self.validation_end_date:][lag_columns].values
+        Y_test = specific_segment[self.validation_end_date:]['sales']
+
+        # Initialize and train the LightGBM model
+        lgb_model = lgb.LGBMRegressor(n_estimators=100, 
+                                       max_depth=10,
+                                       random_state=42,
+                                       learning_rate=0.1)
+        lgb_model.fit(X_train, Y_train)
+
+        # Make predictions
+        y_predict = lgb_model.predict(X_test)
+
+        # Plotting the forecast alongside the actual test data
+        plt.figure(figsize=(12, 6))
+        plt.plot(Y_test.index, y_predict, color='blue', label='Predicted Sales')
+        plt.plot(Y_test.index, Y_test, color='red', label='Actual Sales')
+        plt.title(f'LightGBM-Puredate Sales Forecast vs Actuals for Store {self.store_number} - Product {self.product_type}')
+        plt.xlabel('Date')
+        plt.ylabel('Sales')
+        plt.legend()
+        plt.show()
+
+        # Evaluating the model's performance
+        r2_date_lgb = r2_score(Y_test, y_predict)
+        print("LightGBM r2_score:", r2_date_lgb)
+
+    def lightgbm_EXfeature_predict(self):
+        import lightgbm as lgb
+    
+        # Create lag features
+        # self.create_lag_features(lags)
+        specific_segment = self.segmented_data[(self.store_number, self.product_type)]
+
+        # Prepare the features and target variable
+        # lag_columns = [f'lag_{i}' for i in range(1, lags + 1)]
+        X_train = specific_segment[:self.validation_end_date][['special_offer', 'id', 'store_nbr']].values
+        Y_train = specific_segment[:self.validation_end_date]['sales']
+
+        X_test = specific_segment[self.validation_end_date:][['special_offer', 'id', 'store_nbr']].values
+        Y_test = specific_segment[self.validation_end_date:]['sales']
+
+        # Initialize and train the LightGBM model
+        lgb_model = lgb.LGBMRegressor(n_estimators=100, 
+                                       max_depth=10,
+                                       random_state=42,
+                                       learning_rate=0.1)
+        lgb_model.fit(X_train, Y_train)
+
+        # Make predictions
+        y_predict = lgb_model.predict(X_test)
+
+        # Plotting the forecast alongside the actual test data
+        plt.figure(figsize=(12, 6))
+        plt.plot(Y_test.index, y_predict, color='blue', label='Predicted Sales')
+        plt.plot(Y_test.index, Y_test, color='red', label='Actual Sales')
+        plt.title(f'LightGBM-EXfeatures Sales Forecast vs Actuals for Store {self.store_number} - Product {self.product_type}')
+        plt.xlabel('Date')
+        plt.ylabel('Sales')
+        plt.legend()
+        plt.show()
+
+        # Evaluating the model's performance
+        r2_EXfeature_lgb = r2_score(Y_test, y_predict)
+        print("LightGBM r2_score:", r2_EXfeature_lgb)
 
     
 
